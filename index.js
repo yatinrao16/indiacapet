8a256e2ecdb570fa2cec3adc441d1019d1bfd220
yatin yadav <49709956+yatinrao16@users.noreply.github.com>
2024-04-11 10:46:50 +0530

    last

--- index.js
+++ index.js
@@ -24,8 +24,11 @@ app.get("/api/products", (req, res) => {
       "price": 109.95,
       "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
       "category": "men's clothing",
-      "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-      "imageUrl": "https://img.freepik.com/free-vector/vector-damask-seamless-pattern-background-classical-luxury-old-fashioned-damask-ornament-royal-victorian-seamless-texture-wallpapers-textile-wrapping-exquisite-floral-baroque-template_1217-628.jpg?t=st=1712744525~exp=1712748125~hmac=dcc95b87656c686cbf80b657dfedd42800661f6b97d9bff1a9670207310b94ea&w=996",
+      "image": "https://img.freepik.com/free-photo/restaurant-hall-with-grey-stone-wall-decorated-with-azerbaijani-carpet_140725-8503.jpg?t=st=1712812018~exp=1712815618~hmac=b5e737de819dae07774c268b4c4cc7102f80aae94cdf719b2eebb9126f83863f&w=740",
+      "imageUrl": "",
+      "imageUrl2": "",
+      "imageUrl3": "",
+      "imageUrl4": "",
       "rating": {
         "rate": 3.9,
         "count": 120
@@ -37,8 +40,11 @@ app.get("/api/products", (req, res) => {
       "price": 22.3,
       "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
       "category": "men's clothing",
-      "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+      "image": "https://img.freepik.com/free-photo/modern-living-room-interior-design_23-2150794726.jpg?t=st=1712812072~exp=1712815672~hmac=17a50dc5e6fd71685f7a6f65b48d829af8731004e8ccc6572fdf51f83eb04112&w=740",
       "imageUrl": "",
+      "imageUrl2": "",
+      "imageUrl3": "",
+      "imageUrl4": "",
       "rating": {
         "rate": 4.1,
         "count": 259
@@ -50,8 +56,11 @@ app.get("/api/products", (req, res) => {
       "price": 55.99,
       "description": "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
       "category": "men's clothing",
-      "image": "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+      "image": "https://img.freepik.com/free-photo/pillow-sofa_1203-2939.jpg?t=st=1712812113~exp=1712815713~hmac=1d26312809392213ac2d27a914c48d578958652c0917bbaecce1512c8a1f0fff&w=740",
       "imageUrl": "",
+      "imageUrl2": "",
+      "imageUrl3": "",
+      "imageUrl4": "",
       "rating": {
         "rate": 4.7,
         "count": 500
@@ -63,8 +72,11 @@ app.get("/api/products", (req, res) => {
       "price": 15.99,
       "description": "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
       "category": "men's clothing",
-      "image": "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+      "image": "https://img.freepik.com/free-photo/sofa-decoration-interior-room_74190-3918.jpg?t=st=1712812161~exp=1712815761~hmac=714fd090f7d7fee0a97f982833304590f6088d8261b536454674069570610bc3&w=740",
       "imageUrl": "",
+      "imageUrl2": "",
+      "imageUrl3": "",
+      "imageUrl4": "",
       "rating": {
         "rate": 2.1,
         "count": 430
@@ -76,8 +88,11 @@ app.get("/api/products", (req, res) => {
       "price": 695,
       "description": "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
       "category": "jewelery",
-      "image": "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
+      "image": "https://img.freepik.com/free-photo/decoration-interior_74190-6488.jpg?t=st=1712812204~exp=1712815804~hmac=1fcadf412ca9c06d43ab047a00f2044fa69cd8587a5d93e3a6cc1acbdba5c284&w=740",
       "imageUrl": "",
+      "imageUrl2": "",
+      "imageUrl3": "",
+      "imageUrl4": "",
       "rating": {
         "rate": 4.6,
         "count": 400
@@ -89,8 +104,11 @@ app.get("/api/products", (req, res) => {
       "price": 168,
       "description": "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
       "category": "jewelery",
-      "image": "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
+      "image": "https://img.freepik.com/free-photo/table-sofa-rug-office_23-2147650961.jpg?t=st=1712812301~exp=1712815901~hmac=422d527892c147a3fdcd9d792017af210a8182f1a23332ece3c7cd701ac4b660&w=740",
       "imageUrl": "",
+      "imageUrl2": "",
+      "imageUrl3": "",
+      "imageUrl4": "",
       "rating": {
         "rate": 3.9,
         "count": 70
@@ -102,8 +120,11 @@ app.get("/api/products", (req, res) => {
       "price": 9.99,
       "description": "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day...",
       "category": "jewelery",
-      "image": "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg",
+      "image": "https://img.freepik.com/free-photo/beautiful-luxury-pillow-sofa-decoration-living-room-interior-vintage-light-filter_74190-1688.jpg?t=st=1712812356~exp=1712815956~hmac=1adcd9c0d204bc9603ac1f1262242e24fd1737ac791e51d7f6e97cfbc42261f1&w=740",
       "imageUrl": "",
+      "imageUrl2": "",
+      "imageUrl3": "",
+      "imageUrl4": "",
       "rating": {
         "rate": 3,
         "count": 400
@@ -115,8 +136,11 @@ app.get("/api/products", (req, res) => {
       "price": 10.99,
       "description": "Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel",
       "category": "jewelery",
-      "image": "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg",
+      "image": "https://img.freepik.com/free-photo/comfortable-chair-living-room_1203-1017.jpg?t=st=1712812385~exp=1712815985~hmac=78e801ba89240f84a1df520be83a2471660d880142e5db4c011b95ffed4236f6&w=740",
       "imageUrl": "",
+      "imageUrl2": "",
+      "imageUrl3": "",
+      "imageUrl4": "",
       "rating": {
         "rate": 1.9,
         "count": 100
@@ -128,8 +152,11 @@ app.get("/api/products", (req, res) => {
       "price": 64,
       "description": "USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system",
       "category": "electronics",
-      "image": "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg",
+      "image": "https://img.freepik.com/free-photo/close-up-vintage-armchair-with-cushion_1203-528.jpg?t=st=1712812412~exp=1712816012~hmac=5ef5b46cd09c36bb15cca384ea4fa95d6730f77fd24e96adb8b60feee89ca394&w=740",
       "imageUrl": "",
+      "imageUrl2": "",
+      "imageUrl3": "",
+      "imageUrl4": "",
       "rating": {
         "rate": 3.3,
         "count": 203
@@ -141,8 +168,11 @@ app.get("/api/products", (req, res) => {
       "price": 109,
       "description": "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
       "category": "electronics",
-      "image": "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+      "image": "https://img.freepik.com/free-photo/couch-with-white-orange-cushions_1203-446.jpg?t=st=1712812439~exp=1712816039~hmac=98c19cc40a27430ec41524c2f980b3453597753e0a719d2ed7c68da5dbafb5b8&w=740",
       "imageUrl": "",
+      "imageUrl2": "",
+      "imageUrl3": "",
+      "imageUrl4": "",
       "rating": {
         "rate": 2.9,
         "count": 470
@@ -154,8 +184,11 @@ app.get("/api/products", (req, res) => {
       "price": 109,
       "description": "3D NAND flash are applied to deliver high transfer speeds Remarkable transfer speeds that enable faster bootup and improved overall system performance. The advanced SLC Cache Technology allows performance boost and longer lifespan 7mm slim design suitable for Ultrabooks and Ultra-slim notebooks. Supports TRIM command, Garbage Collection technology, RAID, and ECC (Error Checking & Correction) to provide the optimized performance and enhanced reliability.",
       "category": "electronics",
-      "image": "https://fakestoreapi.com/img/71kWymZ+c+L._AC_SX679_.jpg",
+      "image": "https://img.freepik.com/free-photo/horror-scene-with-eerie-hall_23-2150975090.jpg?t=st=1712812506~exp=1712816106~hmac=a32bd663b38837425399c76ea20c848df6894da782a47eb17829bf06fd26d80c&w=740",
       "imageUrl": "",
+      "imageUrl2": "",
+      "imageUrl3": "",
+      "imageUrl4": "",
       "rating": {
         "rate": 4.8,
         "count": 319
@@ -167,8 +200,11 @@ app.get("/api/products", (req, res) => {
       "price": 114,
       "description": "Expand your PS4 gaming experience, Play anywhere Fast and easy, setup Sleek design with high capacity, 3-year manufacturer's limited warranty",
       "category": "electronics",
-      "image": "https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_.jpg",
+      "image": "https://img.freepik.com/free-photo/view-luxurious-hotel-hallway_23-2150683499.jpg?t=st=1712812531~exp=1712816131~hmac=b0b39c0fe64c01dae293b25f3b059bc10abe02c7797ccb607eb19ffc8f175a4f&w=740",
       "imageUrl": "",
+      "imageUrl2": "",
+      "imageUrl3": "",
+      "imageUrl4": "",
       "rating": {
         "rate": 4.8,
         "count": 400
@@ -180,8 +216,11 @@ app.get("/api/products", (req, res) => {
       "price": 599,
       "description": "21. 5 inches Full HD (1920 x 1080) widescreen IPS display And Radeon free Sync technology. No compatibility for VESA Mount Refresh Rate: 75Hz - Using HDMI port Zero-frame design | ultra-thin | 4ms response time | IPS panel Aspect ratio - 16: 9. Color Supported - 16. 7 million colors. Brightness - 250 nit Tilt angle -5 degree to 15 degree. Horizontal viewing angle-178 degree. Vertical viewing angle-178 degree 75 hertz",
       "category": "electronics",
-      "image": "https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg",
+      "image": "https://img.freepik.com/free-photo/modern-styled-small-entryway_23-2150712853.jpg?t=st=1712812562~exp=1712816162~hmac=0122651f28b78af9c84785cc35adab87ad8f6f07b0fdfccdf90d6577fa52bd2a&w=740",
       "imageUrl": "",
+      "imageUrl2": "",
+      "imageUrl3": "",
+      "imageUrl4": "",
       "rating": {
         "rate": 2.9,
         "count": 250
@@ -195,6 +234,9 @@ app.get("/api/products", (req, res) => {
       "category": "electronics",
       "image": "https://fakestoreapi.com/img/81Zt42ioCgL._AC_SX679_.jpg",
       "imageUrl": "",
+      "imageUrl2": "",
+      "imageUrl3": "",
+      "imageUrl4": "",
       "rating": {
         "rate": 2.2,
         "count": 140
@@ -208,6 +250,9 @@ app.get("/api/products", (req, res) => {
       "category": "women's clothing",
       "image": "https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg",
       "imageUrl": "",
+      "imageUrl2": "",
+      "imageUrl3": "",
+      "imageUrl4": "",
       "rating": {
         "rate": 2.6,
         "count": 235
@@ -221,6 +266,9 @@ app.get("/api/products", (req, res) => {
       "category": "women's clothing",
       "image": "https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg",
       "imageUrl": "",
+      "imageUrl2": "",
+      "imageUrl3": "",
+      "imageUrl4": "",
       "rating": {
         "rate": 2.9,
         "count": 340
@@ -234,6 +282,9 @@ app.get("/api/products", (req, res) => {
       "category": "women's clothing",
       "image": "https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg",
       "imageUrl": "",
+      "imageUrl2": "",
+      "imageUrl3": "",
+      "imageUrl4": "",
       "rating": {
         "rate": 3.8,
         "count": 679
@@ -247,6 +298,9 @@ app.get("/api/products", (req, res) => {
       "category": "women's clothing",
       "image": "https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg",
       "imageUrl": "",
+      "imageUrl2": "",
+      "imageUrl3": "",
+      "imageUrl4": "",
       "rating": {
         "rate": 4.7,
         "count": 130
@@ -260,6 +314,9 @@ app.get("/api/products", (req, res) => {
       "category": "women's clothing",
       "image": "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg",
       "imageUrl": "",
+      "imageUrl2": "",
+      "imageUrl3": "",
+      "imageUrl4": "",
       "rating": {
         "rate": 4.5,
         "count": 146
@@ -273,6 +330,9 @@ app.get("/api/products", (req, res) => {
       "category": "women's clothing",
       "image": "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg",
       "imageUrl": "",
+      "imageUrl2": "",
+      "imageUrl3": "",
+      "imageUrl4": "",
       "rating": {
         "rate": 3.6,
         "count": 145
